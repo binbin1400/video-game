5fa9ca6434aeac794f3fffacec27cac8ed4370b9
binbin1400 <58241327+binbin1400@users.noreply.github.com>
2020-12-26 16:17:45 +0700

    style-css

---
+++ video_game/resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laracast Video Games</title>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+                <div class="flex items-center">
+                        <a href="/">
+                            <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                        </a>
+                    <ul class="flex ml-16 space-x-8">
+                        <li><a href="#" class="hover:text-gray-400"></a>Games</li>
+                        <li><a href="#" class="hover:text-gray-400"></a>Reviews</li>
+                        <li><a href="#" class="hover:text-gray-400"></a>Coming Soon</li>
+                    </ul>
+                </div>
+
+                <div class="flex items-center">
+                    <div class="relative">
+
+                    </div>
+                    <div class="ml-6">
+                        <a href="#"> <img src="/avatar.jpg" class="rounded-full w-8"> </a>
+                    </div>
+                </div>
+        </nav>
+    </header>
+</body>
+</html>
